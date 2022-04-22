08bc07b0d3390687bf37d6b0d976b472f4aada33
gromov <gromovitaly@gmail.com>
2022-04-22 07:37:12 +0300

    Initial Commit

---
+++ FlagsGame/FlagsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsGameApp.swift
+//  FlagsGame
+//
+//  Created by Vitaly Gromov on 4/22/22.
+//
+
+import SwiftUI
+
+@main
+struct FlagsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
